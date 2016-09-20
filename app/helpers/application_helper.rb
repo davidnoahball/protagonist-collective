4f8a2ba3ff82097a109c00775fd51fbf941489fa
davidnoahball <davidnoahball@gmail.com>
2016-09-20 15:28:19 -0700

    Added current-page styling to header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'current' : nil
+
+    content_tag(:a, link_text, :class => class_name, :href => link_path)
+  end
 end
