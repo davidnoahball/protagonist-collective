bfe4d9f91a8ef372a73a554a10f62b464d8e7559
davidnoahball <davidnoahball@gmail.com>
2016-09-20 16:47:13 -0700

    User isn't logged-out after registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def create
     user = User.new(user_params).save
+
+    @user = User.find_by(name: params[:user][:name])
+    return head(:forbidden) unless @user.authenticate(params[:user][:password])
+    session[:user_id] = @user.id
+    
     redirect_to "/"
   end
   def new
