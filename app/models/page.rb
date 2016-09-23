caae5531058310962e37b4ec59a4fd3b7b89b774
davidnoahball <davidnoahball@gmail.com>
2016-09-22 19:56:39 -0700

    Put word and page count on site's index

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,14 +3,18 @@ class Page < ApplicationRecord
   belongs_to :adventure
 
   validates :choice1, :choice2, :end, length: {in: 1..30}, allow_nil: true
-  validates :body, length: {in: 50..300}
+  validates :body, length: {in: 50..1000}
 
   def self.word_count
     total = 0
     Page.all.each do |page|
       if page.body.nil? || page.body.empty? then next end
       total += page.body.split(" ").count
+      if page.end then total += page.end.split(" ").count end
     end
     total
+  end
+  def self.count
+    Page.all.count
   end
 end
