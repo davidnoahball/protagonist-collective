922c09902298e088b6cf0d8b59d77d163e39f610
davidnoahball <davidnoahball@gmail.com>
2016-10-03 19:16:25 -0700

    Refactored some of the Pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,24 +8,12 @@ class PagesController < ApplicationController
     @page = Page.new(adventure_id: params[:adventure_id], parent_id: session[:parent_id], user_id: session[:user_id])
   end
   def create
-    filtered = pages_params
-    filtered.delete_if {|k, v|
-      v.blank?
-    }
-    @page = Page.new(filtered)
-    if filtered[:end] && (filtered[:choice1] || filtered[:choice2])
-      @page.errors.add(:end, :not_valid, message: ": You can't have an ending and choices!")
-    end
-    if !(filtered[:end] || filtered[:choice1] || filtered[:choice2])
-      @page.errors.add(:end, :not_valid, message: ": You need either an ending or choices!")
-    end
-    if (@page.end && !(@page.adventure.critical? || @page.user.ends_available?(@page.adventure)) && User.find(session[:user_id]).usertype == 0)
-      @page.errors.add(:end, :not_valid, message: ": You can't write an ending unless you have written a choices page or the adventure is critical!")
-    end
+    @page = Page.new(pages_params)
+
     if !(session[:user_id])
       @page.errors.add(:body, :not_valid, message: ": You must be logged in to write a new page!")
     end
-    par = Adventure.find(filtered[:adventure_id]).pages.find(filtered[:parent_id])
+    par = @page.adventure.pages.find(@page.parent_id)
     if (par.child1_id != nil && session[:option] == "1") || (par.child2_id != nil && session[:option] == "2")
       return redirect_to "/"
     end
@@ -40,7 +28,8 @@ class PagesController < ApplicationController
     else
       return render :new
     end
-    redirect_to "/adventures/#{@page.adventure_id}/pages/#{@page.id}"
+
+    redirect_to @page.path
   end
   def edit
     if User.find(session[:user_id]).usertype == 0
@@ -52,12 +41,9 @@ class PagesController < ApplicationController
     if User.find(session[:user_id]).usertype == 0
       redirect_to "/"
     end
-    filtered = pages_params
-    filtered.delete_if {|k, v|
-      v.blank?
-    }
+
     @page = Page.find(params[:id])
-    @page.update(filtered)
+    @page.update(pages_params)
     if @page.save
       redirect_to @page.path
     else
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,8 +2,32 @@ class Page < ApplicationRecord
   belongs_to :user
   belongs_to :adventure
 
+  before_validation :filter_empty
+
   validates :choice1, :choice2, :end, length: {in: 1...60}, allow_nil: true
   validates :body, length: {in: 50...1000}
+  validate :validate_endings_choices
+  validate :validate_end_allowed, if: "self.end"
+
+  def validate_endings_choices
+    if self.end && (self.choice1 || self.choice2)
+      errors.add(:base, :not_valid, message: "You can't have an ending and choices!")
+    end
+    if !(self.end || self.choice1 || self.choice2)
+      errors.add(:base, :not_valid, message: "You need either an ending or choices!")
+    end
+  end
+  def validate_end_allowed
+    if !(self.adventure.critical? || self.user.ends_available?(self.adventure))
+      errors.add(:end, :not_valid, message: ": You can't write an ending unless you have written a choices page or the adventure is critical!")
+    end
+  end
+
+  def filter_empty
+    self.attributes.each_pair do |k, v|
+      if v.blank? then self[k] = nil end
+    end
+  end
 
   def self.word_count
     total = 0
