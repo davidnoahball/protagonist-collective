f858e6d9398b98f19c3b220932c1bfb0134c4e9b
davidnoahball <davidnoahball@gmail.com>
2016-10-22 22:11:09 -0700

    Set up dummy files and routes

--- app/assets/javascripts/angular/app.js
+++ app/assets/javascripts/angular/app.js
@@ -20,5 +20,35 @@ angular
           templateUrl: 'site/howitworks.html',
           controller: 'spaThingsController'
         })
+        .state('adventure-index', {
+          url: '/adventures',
+          templateUrl: 'adventure/index.html',
+          controller: 'spaThingsController'
+        })
+        .state('adventure-new', {
+          url: '/adventures/new',
+          templateUrl: 'adventure/new.html',
+          controller: 'spaThingsController'
+        })
+        .state('page-new', {
+          url: '/adventures/:ad_id/pages/new',
+          templateUrl: 'page/new.html',
+          controller: 'spaThingsController'
+        })
+        .state('page-show', {
+          url: '/adventures/:ad_id/pages/:pg_id',
+          templateUrl: 'page/show.html',
+          controller: 'spaThingsController'
+        })
+        .state('login', {
+          url: '/login',
+          templateUrl: 'user/login.html',
+          controller: 'spaThingsController'
+        })
+        .state('user-show', {
+          url: '/users/:us_id',
+          templateUrl: 'user/show.html',
+          controller: 'spaThingsController'
+        })
     }
   ]);
