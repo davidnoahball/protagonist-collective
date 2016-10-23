93fd781d53cb04f09ed798b28f14314830d6cbcf
davidnoahball <davidnoahball@gmail.com>
2016-10-22 18:35:54 -0700

    Built 'second-header' and related directives

---
+++ app/assets/javascripts/angular/directives/second-header.directive.js
@@ -0,0 +1,40 @@
+function secondHeader() {
+  return {
+    transclude: true,
+    templateUrl: 'second-header-template.html'
+  };
+}
+function secondHeaderRight() {
+  return {
+    transclude: true,
+    template: '<div class="secondary-right"><span ng-transclude></span></div>'
+  };
+}
+function secondHeaderCenter() {
+  return {
+    transclude: true,
+    template: '<div class="secondary-center"><span ng-transclude></span></div>'
+  };
+}
+function secondHeaderLeft() {
+  return {
+    transclude: true,
+    template: '<div class="secondary-left"><span ng-transclude></span></div>'
+  };
+}
+ 
+angular
+  .module('protag')
+  .directive('secondHeader', secondHeader)
+
+angular
+  .module('protag')
+  .directive('secondHeaderRight', secondHeaderRight);
+
+angular
+  .module('protag')
+  .directive('secondHeaderCenter', secondHeaderCenter);
+
+angular
+  .module('protag')
+  .directive('secondHeaderLeft', secondHeaderLeft);
